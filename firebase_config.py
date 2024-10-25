2fe0e4bed76b3517e74237864008eaa0e2ac4efb
Abhinaya Shrinivasan <126884981+Abhinaya0122@users.noreply.github.com>
2024-10-25 11:14:20 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, firestore
 
 def initialize_firebase():
     # Provide the path to your Firebase service account key file
-    cred = credentials.Certificate("loginsignup-59e5c-firebase-adminsdk-h86w9-a1acd2374e.json")
+    cred = credentials.Certificate("")
     
     # Initialize Firebase app
     firebase_admin.initialize_app(cred)
